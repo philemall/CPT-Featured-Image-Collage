08230bd50671a1737cde43fcb57a8c8e3ab9fe14
Phil Bennett <phil@snapshotinteractive.com>
2016-09-22 15:40:10 -0500

    Auto-Determine Used Rows

--- cpt-collage.php
+++ cpt-collage.php
@@ -27,10 +27,13 @@ function create_member_collage($post_id) {
 	wp_reset_postdata();
 
 	$num_columns = 10;
-	$num_rows = ceil(sizeof($images) / $num_columns);
+	$occupied_rows = ceil(sizeof($images) / $num_columns);
+	$num_rows = 10;
+	$height = '110';
+	$width = '110';
 
 	if(sizeof($images) % $num_columns  !== 0) {
-			do {
+		do {
 			$filler_images = $num_columns - (sizeof($images) % $num_columns);
 			for($x = 0; $x < sizeof($images); $x++) {
 				if(sizeof($images) % $num_columns  == 0) break;
@@ -39,14 +42,21 @@ function create_member_collage($post_id) {
 		} while (sizeof($images) % $num_columns  !== 0);
 	}
 
+	$image_list = $images;
+	$shuffled = $images;
+	for($row = 0; $row < ($num_rows - $occupied_rows); $row++) {
+		shuffle($shuffled);
+		$image_list = array_merge($image_list, $shuffled);
+	}
+
 	$stack = new Imagick();
 	$stack->setCompressionQuality(62);
-	foreach($images as $image) {
+	foreach($image_list as $image) {
 		$stack->addImage(new Imagick($image));
 	}
 
 
-	$montage = $stack->montageImage(new ImagickDraw(), $num_columns . 'x' . $num_rows, '', 0, '0');
+	$montage = $stack->montageImage(new ImagickDraw(), $num_columns . 'x' . $num_rows, $height . 'x' . $width, 0, '0');
 	$member_image = get_template_directory() . '/css/img/bg_headshots.jpg';
 	$montage->writeImage($member_image);
 
